1ac603a4b13ae324c0ce27005eb97109ec716e5f
Juergen Edelbluth <juergen@edelbluth.eu>
2014-10-05 10:34:32 +0200

    Basic Keypad Usage: Basic Setup

---
+++ experiments/keypad/Basic_Keypad_Usage/Basic_Keypad_Usage.ino
@@ -0,0 +1,20 @@
+/*
+ * Basic_Keypad_Usage.ino
+ *
+ * Created: 10/5/2014 9:12:50 AM
+ * Author: juergen
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
